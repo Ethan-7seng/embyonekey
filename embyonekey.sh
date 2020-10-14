bf9bc0a9fd649f713cee49fc4a2fcd2bfc30f228
Ethan-7seng <70874195+Ethan-7seng@users.noreply.github.com>
2020-10-14 15:58:06 +0800

    Update embyonekey.sh

--- embyonekey.sh
+++ embyonekey.sh
@@ -3,6 +3,6 @@ cd /var/packages/EmbyServer/target/mono/bin
 curl https://raw.githubusercontent.com/s1oz/embyonekey/master/mb3admin.com.cert.pem >> /etc/ssl/certs/ca-certificates.crt
 ./cert-sync /etc/ssl/certs/ca-certificates.crt
 cd /usr/local/etc/nginx/conf.d/*-*-*-*
-wget https://raw.githubusercontent.com/s1oz/embyonekey/master/user.conf
+wget https://raw.githubusercontent.com/Ethan-7seng/embyonekey/master/user.conf
 nginx -s reload
 
